b7243384c9e87b79fd759c15565234cb1ad94b56
Michael Milewski <saramic@gmail.com>
2025-04-26 10:45:58 +1000

    LAB 04 - START AI generate body in background

--- app/controllers/generated_messages_controller.rb
+++ app/controllers/generated_messages_controller.rb
@@ -25,6 +25,20 @@ class GeneratedMessagesController < ApplicationController
       @generated_message.message.body,
     )
 
+    # LAB 04.2
+    # ========
+    # Call the generate text in background job
+    #
+    # HINT:
+    #   - https://guides.rubyonrails.org/active_job_basics.html#enqueue-the-job
+    #   - app/jobs/generated_message_job.rb
+    #   - GeneratedMessageJob.perform_later( <model> )
+    #
+    # STEPS:
+    #   1. call the background job
+    #   2. update the generated message body
+    #   3. continue to LAB 04.3 to fix the notification
+
     if @generated_message.save
       # LAB 02.4
       # ========
@@ -38,6 +52,17 @@ class GeneratedMessagesController < ApplicationController
       #   2. if all goes well the notification will be correct, the Generated
       #      Text will be updated and the feature spec should be passing.
       #      `bin/rspec spec/features/user_adds_message_to_guestbook_spec.rb`
+
+      # LAB 04.3
+      # ========
+      # Need to tell the suer that the text is "being generated" and NOT that it has "successfully generated"
+      #
+      # HINT:
+      #   - https://guides.rubyonrails.org/action_controller_overview.html#the-flash
+      #
+      # STEPS:
+      #   1. fix the flash notification messagecheck if the body is present
+      #   2. continue to LAB 04.3 to save and correctly notify
 
       flash.now[:notice] = "AI text successfully generated."
       render :new, status: :see_other
@@ -63,6 +88,22 @@ class GeneratedMessagesController < ApplicationController
     # STEPS:
     #   1. update the model with the parameters
     #   2. test manually in browser that you can update the generated message
+
+    # LAB 04.4
+    # ========
+    # Need to check if the body has been succesfully generated, if not, say it
+    # is being generated, if yes let the user know it has been successfully
+    # generated.
+    #
+    # HINT:
+    #   - check if body is present @generated_message.body.present?
+    #   - check if the "refresh" button was pressed `if params[:refresh]``
+    #
+    # STEPS:
+    #   1. check if the body is present
+    #   2. check if the refresh button was pressed
+    #   3. if YES for 1. and 2. above, return still generated
+    #   4. otherwise let the user know it has been generated
 
     if @generated_message.update(generated_message_params[:generated_message])
       redirect_to \
--- spec/features/user_adds_message_to_guestbook_spec.rb
+++ spec/features/user_adds_message_to_guestbook_spec.rb
@@ -112,6 +112,47 @@ feature "User adds message to guestbook", :js do
         guestbook.generate_ai_body.click
       end
 
+      # LAB 04.1
+      # ========
+      # Calling the AI generator can take some time, in this lab we want to
+      # respond to the user immediately that the AI is generating the text and
+      # allow the user to check back to see if it has been generated. The below
+      # spec will fail as it will wait till the AI text has been generated and
+      # not return immediately notifying the user it is running in the
+      # background. This notification will need to be fixed in LAB 04.2 .
+      #
+      # STEPS:
+      #   1. Uncomment the following code
+      #   2. Run the spec - see 1 pending spec
+      #      `bin/rspec spec/features/user_adds_message_to_guestbook_spec.rb`
+
+      # Then "the user is notified that the AI is generating the text" do
+      #   # NOTE: as this is asynchronous, we need to "wait" with "have_content"
+      #   # to make sure the page has reloaded succesfully, before we assert that
+      #   # the notification is what we want. This is a quirk of the
+      #   # Webdriver/Capybara/SitePrism setup we have decided to use and may
+      #   # differ on other platforms.
+      #   pending "AI text being generated in background job"
+      #   expect(page).to have_content "AI text is being generated."
+      #   expect(guestbook.notification).to eq "AI text is being generated."
+      # end
+
+      # Then "When they refresh" do
+      #   guestbook.refresh.click
+      # end
+
+      # Then "the user is still notified text generation is in progress" do
+      #   expect(guestbook.notification).to eq "AI text is being generated."
+      # end
+
+      # When "AI has finished generating the response" do
+      #   perform_enqueued_jobs
+      # end
+
+      # And "they refresh" do
+      #   guestbook.refresh.click
+      # end
+
       Then "the generated AI text is displayed" do
         binding.irb if ENV.fetch("SPEC_PAUSE", false) # rubocop:disable Lint/Debugger
         SitePrism::Waiter.wait_until_true {
